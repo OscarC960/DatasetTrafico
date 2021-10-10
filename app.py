089221008499de1479678a3b8bf85f6cd54db0b6
Diefex <diefex57@gmail.com>
2021-10-10 14:52:03 -0500

    Input fechas

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dash.dependencies import Input, Output
 
 import plotly.express as px
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import numpy as np
 
 from separar import *
@@ -24,67 +24,57 @@ except:
     Titulo = "No se encontro el Dataset"
     error = True
 
-# -------- VER TRAFICO DE DATOS SUMADO EN UN RANGO DE FECHAS --------
-# Definir un rango de fechas
-fecha_inicial = datetime(2020,5,20)
-fecha_final = datetime(2020,5,30)
-rango_fechas = [fecha_inicial + timedelta(days=d) for d in range((fecha_final - fecha_inicial).days + 1)] 
-rango_fechas = [f.strftime('%#d-%#m-%Y') for f in rango_fechas]
+# ----- FUNCIONES GENERADORAS DE GRAFICOS -----
 
-# Separar y agurpar los datos para el grafico
-df_gr1 = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
-df_gr1 = separar_columnas(df_gr1, ['PROVEEDOR', 'TRAFICO_DATOS_INTERNACIONAL_GB']) # separar las columnas que necesito
-df_gr1 = df_gr1.groupby("PROVEEDOR").sum() # agrupar por proveedor y sumar el trafico
-df_gr1 = df_gr1.rename_axis('PROVEEDOR').reset_index()
+def gr_tr_total(rango_fechas):
+    # Separar y agurpar los datos para el grafico
+    df_gr = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
+    df_gr = separar_columnas(df_gr, ['PROVEEDOR', 'TRAFICO_DATOS_INTERNACIONAL_GB']) # separar las columnas que necesito
+    df_gr = df_gr.groupby("PROVEEDOR").sum() # agrupar por proveedor y sumar el trafico
+    df_gr = df_gr.rename_axis('PROVEEDOR').reset_index()
 
-# Generar grafica de barras
-grafico = gr_barras(df_gr1, 'PROVEEDOR', 'TRAFICO_DATOS_INTERNACIONAL_GB')
+    # Generar grafica de barras
+    return gr_barras(df_gr, 'PROVEEDOR', 'TRAFICO_DATOS_INTERNACIONAL_GB')
 
 
-# -------- VER HORA PICO UN RANGO DE FECHAS --------
-# Separar los datos para el grafico
-df_gr2 = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
-df_gr2 = separar_columnas(df_gr2, ['PROVEEDOR', 'FECHA_DEL_DIA_DE_TRAFICO', 'HORA_PICO']) # separar las columnas que necesito
-df_gr2['HORA_PICO'] = pd.to_datetime(df_gr2['HORA_PICO'], format="%H:%M") # formatear la hora en formato datetime
+def gr_hr_pico(rango_fechas):
+    # Separar los datos para el grafico
+    df_gr = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
+    df_gr = separar_columnas(df_gr, ['PROVEEDOR', 'FECHA_DEL_DIA_DE_TRAFICO', 'HORA_PICO']) # separar las columnas que necesito
+    df_gr['HORA_PICO'] = pd.to_datetime(df_gr['HORA_PICO'], format="%H:%M") # formatear la hora en formato datetime
 
-# Generar grafica de puntos
-grafico2 = gr_puntos(df_gr2, 'FECHA_DEL_DIA_DE_TRAFICO', 'HORA_PICO', 'PROVEEDOR')
+    # Generar grafica de puntos
+    return gr_puntos(df_gr, 'FECHA_DEL_DIA_DE_TRAFICO', 'HORA_PICO', 'PROVEEDOR')
 
 
-# -------- VER TRAFICOS DE DATOS AGRUPADOS EN UN RANGO DE FECHAS --------
-# Separar los datos para el grafico
-df_gr3 = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
-df_gr3 = separar_columnas(df_gr3, ['PROVEEDOR', 'FECHA_DEL_DIA_DE_TRAFICO', 'TRAFICO_DATOS_LOCAL_GB']) # separar las columnas que necesito
+def gr_tr_diario(rango_fechas):
+    # Separar los datos para el grafico
+    df_gr = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
+    df_gr = separar_columnas(df_gr, ['PROVEEDOR', 'FECHA_DEL_DIA_DE_TRAFICO', 'TRAFICO_DATOS_LOCAL_GB']) # separar las columnas que necesito
 
-# Generar grafica de puntos
-grafico3 = gr_lineas(df_gr3, 'FECHA_DEL_DIA_DE_TRAFICO', 'TRAFICO_DATOS_LOCAL_GB', 'PROVEEDOR')
+    # Generar grafica de puntos
+    return gr_lineas(df_gr, 'FECHA_DEL_DIA_DE_TRAFICO', 'TRAFICO_DATOS_LOCAL_GB', 'PROVEEDOR')
 
-# Separar y agurpar los datos para el grafico 4
-df_gr4 = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
-df_gr4 = separar_columnas(df_gr4, ['PROVEEDOR', 'TRAFICO_DATOS_INTERNACIONAL_GB','TRAFICO_DATOS_NAP_COLOMBIA_GB','TRAFICO_DATOS_ACUERDOS_DE_TRANSITO_O_PEERING_DIRECTO_GB','TRAFICO_DATOS_LOCAL_GB','TRAFICO_DATOS_TOTAL_GB']) # separar las columnas que necesito
-df_gr4 = df_gr4.groupby("PROVEEDOR").sum() # agrupar por proveedor y sumar el trafico
-df_gr4 = df_gr4.rename_axis('PROVEEDOR').reset_index()
 
-# Generar grafica de barras agrupadas
-grafico4 = gr_barras_agr(df_gr4, 'PROVEEDOR', ['TRAFICO_DATOS_INTERNACIONAL_GB','TRAFICO_DATOS_NAP_COLOMBIA_GB','TRAFICO_DATOS_ACUERDOS_DE_TRANSITO_O_PEERING_DIRECTO_GB','TRAFICO_DATOS_LOCAL_GB'])
+def gr_tr_detallado(rango_fechas):
+    df_gr = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
+    df_gr = separar_columnas(df_gr, ['PROVEEDOR', 'TRAFICO_DATOS_INTERNACIONAL_GB','TRAFICO_DATOS_NAP_COLOMBIA_GB','TRAFICO_DATOS_ACUERDOS_DE_TRANSITO_O_PEERING_DIRECTO_GB','TRAFICO_DATOS_LOCAL_GB','TRAFICO_DATOS_TOTAL_GB']) # separar las columnas que necesito
+    df_gr = df_gr.groupby("PROVEEDOR").sum() # agrupar por proveedor y sumar el trafico
+    df_gr = df_gr.rename_axis('PROVEEDOR').reset_index()
 
-#-------- VER TRAFICO DE DATOS TOTAL EN UN RANGO DE FECHAS --------
-#Separar los datos para el grafico
-df_gr5 = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # separar filas en ese rango de fechas
-df_gr5 = separar_columnas(df_gr5, ['PROVEEDOR', 'FECHA_DEL_DIA_DE_TRAFICO', 'TRAFICO_DATOS_ACUERDOS_DE_TRANSITO_O_PEERING_DIRECTO_GB']) # separar las columnas que necesito
+    # Generar grafica de barras agrupadas
+    return gr_barras_agr(df_gr, 'PROVEEDOR', ['TRAFICO_DATOS_INTERNACIONAL_GB','TRAFICO_DATOS_NAP_COLOMBIA_GB','TRAFICO_DATOS_ACUERDOS_DE_TRANSITO_O_PEERING_DIRECTO_GB','TRAFICO_DATOS_LOCAL_GB'])
 
-#Generar grafica de puntos para trafico de datos con acuerdos
-grafico5 = gr_lineas(df_gr5, 'FECHA_DEL_DIA_DE_TRAFICO', 'TRAFICO_DATOS_ACUERDOS_DE_TRANSITO_O_PEERING_DIRECTO_GB', 'PROVEEDOR')
 
-# -------- VER ENTRADAS DEL DATASET --------
-# filtrar el dataset
-df_tbl = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # filtar un rango de fechas
-df_tbl = separar_filas(df_tbl, 'PROVEEDOR', ['MOVISTAR', 'ETB']) # filtrar unos proveedores
-rango_trafico = [r for r in range(300000, 600000)] 
-df_tbl = separar_filas(df_tbl, 'TRAFICO_DATOS_LOCAL_GB', rango_trafico) # filtrar un rango de trafico
+def tbl_dataset(rango_fechas):
+    # filtrar el dataset
+    df_tbl = separar_filas(data_frame, 'FECHA_DEL_DIA_DE_TRAFICO', rango_fechas) # filtar un rango de fechas
+    df_tbl = separar_filas(df_tbl, 'PROVEEDOR', ['MOVISTAR', 'ETB']) # filtrar unos proveedores
+    rango_trafico = [r for r in range(300000, 600000)] 
+    df_tbl = separar_filas(df_tbl, 'TRAFICO_DATOS_LOCAL_GB', rango_trafico) # filtrar un rango de trafico
 
-# Generar tabla del dataset
-dataset = tabla(df_tbl)
+    # Generar tabla del dataset
+    return tabla(df_tbl)
 
 
 # ----- ESTILOS -----
@@ -120,34 +110,75 @@ sidebar = html.Div([
 ], style=SIDEBAR_STYLE, className="text-white bg-dark")
 
 # ----- CONTENIDO -----
-content = html.Div(id="page-content", style=CONTENT_STYLE)
-mybtn = html.Button('botonXD', className='btn btn-primary')
+content = html.Div([
+    html.H2(id='titulo'),
+    dcc.DatePickerRange(
+        id='input_fecha',
+        min_date_allowed=date(2020, 3, 30),
+        max_date_allowed=date(2021, 9, 21),
+        initial_visible_month=date(2020, 5, 20),
+        
+        start_date=date(2020, 5, 20),
+        end_date=date(2020, 5, 30)
+    ),
+    html.Div(id='grafico')
+], id="page-content", style=CONTENT_STYLE)
+
 
 # ----- CREACION DE LA APP -----
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 # ----- CALLBACKS -----
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+@app.callback(
+    Output("titulo", "children"), 
+    Input("url", "pathname"))
 def render_page_content(pathname):
     if pathname == "/":
-        return grafico
+        return 'Trafico total en un rango de fechas'
     elif pathname == "/Hora-Pico":
-        return grafico2
+        return 'Hora pico de cada dia en un rango de fechas'
     elif pathname == "/Trafico-Diario":
-        return grafico3
+        return 'Trafico diario en un rango de fechas'
     elif pathname == "/Trafico-Detallado":
-        return grafico4
+        return 'Todos los tipos de trafico en un rango de fechas'
     elif pathname == "/Dataset":
-        return dataset
+        return 'Registros del dataset'
     
-    return dbc.Jumbotron(
-        [
-            html.H1("404: Not found", className="text-danger"),
-            html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
-        ]
-    )
+    return html.Div('Error al cargar el sitio')
+
+@app.callback(
+    Output('grafico', 'children'),
+    Input('input_fecha', 'start_date'),
+    Input('input_fecha', 'end_date'),
+    Input("url", "pathname"))
+def update_output(start_date, end_date, pathname):
+
+    if start_date is not None:
+        fecha_inicial = date.fromisoformat(start_date)
+    if end_date is not None:
+        fecha_final = date.fromisoformat(end_date)
+    
+    # crear el rango de fechas
+    rango_fechas = [fecha_inicial + timedelta(days=d) for d in range((fecha_final - fecha_inicial).days + 1)] 
+    rango_fechas = [f.strftime('%#d-%#m-%Y') for f in rango_fechas]
+
+    if pathname == "/":
+        return gr_tr_total(rango_fechas)
+    elif pathname == "/Hora-Pico":
+        return gr_hr_pico(rango_fechas)
+    elif pathname == "/Trafico-Diario":
+        return gr_tr_diario(rango_fechas)
+    elif pathname == "/Trafico-Detallado":
+        return gr_tr_detallado(rango_fechas)
+    elif pathname == "/Dataset":
+        return tbl_dataset(rango_fechas)
+    
+    return dbc.Jumbotron([
+        html.H1("404: sitio no encontrado", className="text-danger"),
+        html.Hr(),
+        html.P(f"El sitio {pathname} no se fue encontrado..."),
+    ])
 
 # ----- CORRER APP -----
 if __name__ == '__main__':
